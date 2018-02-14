c95ecfc082eac3134bc3a3fdf3d5b2723df6444b
EC2 Default User <ec2-user@ip-10-0-0-80.ec2.internal>
2018-02-14 20:43:31 +0000

    Add libraries

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ find . -name ".DS_Store" -depth -exec rm {} \;
 rm -rf dist package.zip
 mkdir -p dist
 
-pip2 install -r requirements.txt -t ./dist/
+pip install -r requirements.txt -t ./dist/
 cp *.py dist
 
-pushd dist; zip -r package.zip *; popd
+pushd dist; zip -r -9 package.zip *; popd
 mv dist/package.zip .
 
 exit 0
